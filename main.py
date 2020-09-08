7bfcc4944f5e2b2dd86fb41dfbb3d0d852d2880e
Glassy1982 <cglass1982@outlook.com>
2020-09-08 14:44:02 +0000

    added about, whyus, testimonials, contact, mealprep htmls files

--- main.py
+++ main.py
@@ -9,5 +9,29 @@ def index():
     return render_template('index.html')
 
 
+@app.route ('/whyus')
+def whyus():
+    return render_template('whyus.html')
+
+
+@app.route ('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route ('/testimonials')
+def testimonials():
+    return render_template('testimonials.html')
+
+
+@app.route ('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route ('/mealprep')
+def mealprep():
+    return render_template('mealprep.html')
+
 if __name__ == "__main__":
     app.run()
