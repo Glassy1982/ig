83a300ba8718904553da4763d09f57589cf6894a
CGlassy1982 <cglass1982@outlook.com>
2020-09-18 14:06:05 +0100

    database updating as entries are entered in form

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 conn = psycopg2.connect('host=ec2-52-0-155-79.compute-1.amazonaws.com user=ssukjbptgzsada password=dacde918826aac7e0ee3680078d5487e3a6808ba18cab5515f6a55c0e69fdc09 dbname=daq9525pn99tuo')
 db = conn.cursor()
 
-emails =[]
 
 @app.route ('/')
 def index():
@@ -19,12 +18,15 @@ def index():
 def formSuccess():
     
     if request.method=="POST":
-        db.execute("SELECT * FROM emails")
-        mail = db.fetchall()
+       
+        name = request.form.get('name')
         email = request.form.get('email')
-        if not emails.__contains__(email):
-            emails.append(email)
-    return render_template('formSuccess.html', mail=mail, emails=emails)
+        db.execute('INSERT INTO emails VALUES(3, %s, %s)', (name, email))
+        db.execute("SELECT * FROM emails")
+        conn.commit()
+        mail = db.fetchall()
+        
+    return render_template('formSuccess.html', mail=mail)
     
 
 @app.route ('/whyus')
